402ede2b947715855a6dcba21372fd9a2a9823ed
Vitalis-hub <Michealvitalis123@gmail.com>
2022-06-15 19:44:59 -0500

    Committing changes to addTodo and deleteTodo files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,15 +16,30 @@ class Home extends Component {
   addTodo = (todo) => {
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
+    // To avoid having dup values, we use the Math.random()function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    const exists = this.state.todos.find(t => t.content === todo.content)
+    if(exists != null) {
+      return
+    }
+    else{
+        todo.id = Math.random();
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
   render() {
@@ -33,10 +48,10 @@ class Home extends Component {
         <h1>Todo's </h1>
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
-        <AddTodo addTodo={this.addTodo} />
+        <AddTodo addTodo={this.addTodo}/>
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos}  deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
